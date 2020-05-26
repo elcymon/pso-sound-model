36af1f8d7427995d336276b4bec08c7040fb403d
elcymon <elcymon@gmail.com>
2020-05-26 11:36:34 +0100

    modify to use inverse square law

--- psoSoundModel.py
+++ psoSoundModel.py
@@ -5,46 +5,39 @@ import pandas as pd
 random.seed()
 # random.uniform(mn,mx)
 # random.gauss(mu,sigma) # mu = 0 and signma should be determined
-velocityBound = pd.DataFrame([[0,-0.2,0,0],[200,0.2,10,0.1]],index=['min','max'],columns=['a0','alpha','ae','sigma'])
-particleDimensionBound = pd.DataFrame([[0,-2,0,0],[2000,2,100,1]],index=['min','max'],columns=['a0','alpha','ae','sigma'])
+velocityBound = pd.DataFrame([[0],[200]],index=['min','max'],columns=['K'])
+particleDimensionBound = pd.DataFrame([[0],[2000]],index=['min','max'],columns=['K'])
 distanceIntensityDF = pd.read_csv('distance-soundIntensity-data.csv')
-columns = ['a0','alpha','ae','sigma']
+columns = ['K']
 
-def computeIntensity(a0,alpha,d,ae,sigma):
-    return (a0 * np.exp(-alpha * d) + ae) * (1 - random.gauss(0,sigma))
-
-def computeFitness(x,distanceIntensityDF):
-    a0,alpha,ae,sigma = x
-    particleIntensityComputation = computeIntensity(a0,alpha,distanceIntensityDF['distance'],ae,sigma)
+def computeIntensity(K,d):
+    return K / (d * d)
+ 
+def computeFitness(K,distanceIntensityDF):
+    particleIntensityComputation = computeIntensity(K,distanceIntensityDF['distance'])
     errors = distanceIntensityDF['soundIntensity'] - particleIntensityComputation
     squareErrors = errors**2
     
     return sum(squareErrors)
 
-def generateParticles(population,particleDimensionBound,columns=['a0','alpha','ae','sigma']):
+def generateParticles(population,particleDimensionBound,columns=['K']):
     pDim = particleDimensionBound
     
     df = pd.DataFrame(columns=columns,index=np.arange(population))
-    df['a0'] = np.random.uniform(pDim.loc['min','a0'],pDim.loc['max','a0'],population)
-    df['alpha'] = np.random.uniform(pDim.loc['min','alpha'],pDim.loc['max','alpha'],population)
-    df['ae'] = np.random.uniform(pDim.loc['min','ae'],pDim.loc['max','ae'],population)
-    df['sigma'] = np.random.uniform(pDim.loc['min','sigma'],pDim.loc['max','sigma'],population)
+    df['K'] = np.random.uniform(pDim.loc['min','K'],pDim.loc['max','K'],population)
     return df
 def generateParticlesTuple(population,particleDimensionBound):
     pDim = particleDimensionBound
     
-    df = pd.DataFrame(columns = ['xi','fitness','personalBest','personalBestFitness','indexOfBestNeighbour',],index=np.arange(population))
-    df['xi'] = [(np.random.uniform(pDim.loc['min','a0'],pDim.loc['max','a0']),\
-                  np.random.uniform(pDim.loc['min','alpha'],pDim.loc['max','alpha']),\
-                  np.random.uniform(pDim.loc['min','ae'],pDim.loc['max','ae']),\
-                  np.random.uniform(pDim.loc['min','sigma'],pDim.loc['max','sigma'])) for i in np.arange(population)]#sigma
+    df = pd.DataFrame(columns = ['K','fitness','personalBest','personalBestFitness','indexOfBestNeighbour',],index=np.arange(population))
+    df['K'] = [(np.random.uniform(pDim.loc['min','K'],pDim.loc['max','K'])])) for i in np.arange(population)]#sigma
     return df
     
 def psoSteps(population,particleDimensionBound,velocityBound,columns,distanceIntensityDF):
     #generate particles
     particlesDF = generateParticlesTuple(population,particleDimensionBound)
-    particlesDF['fitness'] = particlesDF.apply(lambda x: computeFitness(x['xi'],distanceIntensityDF),axis=1)
-    particlesDF['personalBest'] = particlesDF['xi']
+    particlesDF['fitness'] = particlesDF.apply(lambda x: computeFitness(x['K'],distanceIntensityDF),aKs=1)
+    particlesDF['personalBest'] = particlesDF['K']
     particlesDF['personalBestFitness'] = particlesDF['fitness']
     globalBest = particlesDF.loc[particlesDF['personalBestFitness'] == particlesDF['personalBestFitness'].min(),:]
     particlesDF['indexOfBestNeighbour'] = globalBest.index[0]
